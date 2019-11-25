a3b67b9ae8f79125a5a22ad0469a3a95130d1c37
jgcardelus <jgcardelus@gmail.com>
2019-11-25 21:10:36 +0100

    Adding Windows capabilities.

--- compiler.py
+++ compiler.py
@@ -1,6 +1,6 @@
 import os
 import sys
-    
+import platform
 
 path = 'templates/'
 components = 'static/components'
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import platform
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -10,8 +11,17 @@ from selenium.webdriver.support import expected_conditions as EC
 DEBUG = True
 port = 8080
 
-# SELENIUM DRIVER
-driver_path = os.path.join(os.getcwd(), 'chrome-driver', 'chromedriver')
-# data_dir_path = os.path.join(os.getcwd(), 'chrome-driver', 'chrome-preferences')
+chromedriver_name = "chromedriver"
+
 data_dir_path = '/home/jgcardelus/Desktop/coding-projects/cosmos-tv-settings/chrome-preferences'
 settings_path = '/home/jgcardelus/Desktop/coding-projects/cosmos-tv-settings/settings.json'
+
+system = platform.system()
+if platform == "Windows":
+    chromedriver_name = "chromedriver.exe"
+    data_dir_path = "C:/Users/jgcar/Desktop/Coding/AAA_MixedProjects/cosmos-tv-settings"
+    settings_path = "C:/Users/jgcar/Desktop/Coding/AAA_MixedProjects/settings.json"
+
+
+# SELENIUM DRIVER
+driver_path = os.path.join(os.getcwd(), 'chrome-driver', chromedriver_name)
